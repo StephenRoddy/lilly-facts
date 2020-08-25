86f5c1833c38399aef55170f7853af66c35a5dc5
Stephen Roddy <roddyst@tcd.ie>
2020-08-25 13:49:19 +0100

    Updates

---
+++ precache-manifest.6c83071454419000a6cd15a88c0bed16.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a7bba0adc34142bfd1a8850254a3dcd",
+    "url": "/lilly-facts/index.html"
+  },
+  {
+    "revision": "70c1079918c1ca5940f1",
+    "url": "/lilly-facts/static/css/main.afa59202.chunk.css"
+  },
+  {
+    "revision": "ffe25ead8ba5603048df",
+    "url": "/lilly-facts/static/js/2.1eb48a51.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lilly-facts/static/js/2.1eb48a51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70c1079918c1ca5940f1",
+    "url": "/lilly-facts/static/js/main.f40b013d.chunk.js"
+  },
+  {
+    "revision": "4ac2848784e7890073f2",
+    "url": "/lilly-facts/static/js/runtime-main.7df9aca5.js"
+  },
+  {
+    "revision": "f89a5e065b69c5231be35c2b33957c6f",
+    "url": "/lilly-facts/static/media/Logo.f89a5e06.png"
+  },
+  {
+    "revision": "5b4c484b39834d7128dfac833f522787",
+    "url": "/lilly-facts/static/media/lilly.5b4c484b.jpg"
+  }
+]);
